7114a0d531b5e1472ed97b44a9f3f24c05cfb1f9
yifeng <493788939lizhaogai@163.com>
2017-03-03 13:50:34 +0800

    return the promise

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function (options) {
     this.phoneSender = new PhoneSender(options.appKey, options.appSecret, options);
     this.send = function (code, to, type) {
         if (type == 'phone') {
-            this.phoneSender.send(code, to);
+            return this.phoneSender.send(code, to);
         } else {
 
         }
