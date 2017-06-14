7148fa49b9bb5832e94a5dab0655f2b5e5ff9806
tangxiaogang <715709084@qq.com>
2017-06-14 18:29:46 +0800

    init

--- mobileMessageSender.js
+++ mobileMessageSender.js
@@ -5,29 +5,21 @@ const PromissA = require('bluebird')
 module.exports = function (appKey, appSecret, options) {
     this.sender = new PhoneMessageSender(appKey, appSecret);
     this.send = function (par, mobile, sendType) {
-        return PromissA.fromCallback(cb => this.sender.smsSend({
+        this.sender.smsSend({
             sms_free_sign_name: (options && options['sign']),
             sms_param: par,
             rec_num: mobile,
             sms_template_code: (options && options['templates'][sendType])
-        }, cb));
-// return new Promise((resolve, reject) => {
-//     console.log((options && options['templates'][sendType]));
-//      sendResult => {
-//         console.log(sendResult);
-//         if (sendResult['error_response']) {
-//           return  reject(errs.create({
-//                 code: 'SEND_VALID_CODE_FAILED',
-//                 status: 401,
-//                 statusCode: 401,
-//                 message: 'send failed'
-//             }));
-//         } else {
-//            return resolve();
-//         }
-//     });
-// });
+        }, (result) => {
+            if (result['error_response'])
+                throw  errs.create({
+                    code: 'SEND_VERIFY_CODE_FAILED',
+                    status: 401,
+                    statusCode: 401,
+                    message: 'send failed'
+                });
+            return PromissA.resolve();
+        });
     }
     ;
-}
-;
+};
